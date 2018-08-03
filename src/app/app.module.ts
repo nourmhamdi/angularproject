3df49064a888370fb91ab220874f38c26f3305e9
MHAMDI NOUR <nourmhamdi1995>
2018-08-03 16:35:01 +0200

    first commit
    Change-Id: I33222a0f7fc36730197049b1d333eade34771f71
    Signed-off-by: MHAMDI NOUR <nourmhamdi1995>

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import{ FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+@NgModule({
+  declarations: [ // here we declare our new component  , we have to import them from were they are defined 
+    AppComponent,
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, // pour pouvoir utilisé ngModel
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({ // component configuration 
+  selector:'app-server', //have to be unique it allows to use the component us an html element 
+  templateUrl:'./server.component.html',
+}) // to make angular know it's a component and not a normal ts class 
+export class ServerComponent { //typeScript class 
+}
