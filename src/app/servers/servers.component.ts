6339e8d374bc0cbb3243dc62b8ea44bf8a673355
MHAMDI NOUR <nourmhamdi1995>
2018-08-09 14:24:35 +0200

    newwwwww

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector:'app-servers', //to call we use the element name
-  //  because the selector is a an element 
+
   //selector: '[app-servers]', //to call we use the div
   //  because the selector is an attribute and not an element 
  //selector: '.app-servers', //to call we use the div
   //  because the selector is a class 
-  template: '<app-server></app-server><app-server></app-server>',   
+      selector:'app-servers', //to call we use the element name
+  //  because the selector is a an element 
+ // template: '<app-server></app-server><app-server></app-server>', 
+  templateUrl: './servers.component.html',  
   // we can define the template directly here 
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
+  allowNewServer= false;
+  serverCreationStatus='No server was created!';
+  serverName='TestServer';
+  serverCreated=false;
+  constructor() { 
+  setTimeout(()=>{this.allowNewServer=true},2000)
+  }
   ngOnInit() {
+  }
+  onCreateServer(){
+    this.serverCreated=true;
+  this.serverCreationStatus='Server was created! Name is '+this.serverName;
+  }
+  onUpdateServerName(event: any){
+  console.log(event);
+    this.serverName= (<HTMLInputElement> event.target).value;
   }
 
 }
